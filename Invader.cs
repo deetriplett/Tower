61945c56d850e3a7bbd2016f68bbbd12a26087ce
Dawn Monroe <deetriplett@gmail.com>
2017-02-15 18:59:30 -0500

    Add files via upload

--- Invader.cs
+++ Invader.cs
@@ -5,18 +5,13 @@ namespace TreehouseDefense
         private readonly Path _path;
         private int _pathStep = 0;
         
-        public MapLocation Location { get; private set; }
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
         
         public Invader(Path path)
         {
             _path = path;
-            Location = _path.GetLocationAt(_pathStep);
         }
         
-        public void Move()
-        {
-            _pathStep += 1;
-            Location = _path.GetLocationAt(_pathStep);
-        }
+        public void Move() => _pathStep += 1;
     }
 }
