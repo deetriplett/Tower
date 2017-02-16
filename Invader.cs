d54cbdf72d4e4664cc01c76d400cb270a5592395
Dawn Monroe <deetriplett@gmail.com>
2017-02-15 19:43:45 -0500

    Add files via upload

--- Invader.cs
+++ Invader.cs
@@ -5,18 +5,27 @@ namespace TreehouseDefense
         private readonly Path _path;
         private int _pathStep = 0;
         
-        public MapLocation Location { get; private set; }
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
+        
+        public int Health { get; private set; } = 2;
+        
+        // True if the invader has reached the end of the path
+        public bool HasScored { get { return _pathStep >= _path.Length; } }
+        
+        public bool IsNeutralized => Health <= 0;
+        
+        public bool IsActive => !(IsNeutralized || HasScored);
         
         public Invader(Path path)
         {
             _path = path;
-            Location = _path.GetLocationAt(_pathStep);
         }
         
-        public void Move()
+        public void Move() => _pathStep += 1;
+        
+        public void DecreaseHealth(int factor)
         {
-            _pathStep += 1;
-            Location = _path.GetLocationAt(_pathStep);
+            Health -= factor;
         }
     }
 }
