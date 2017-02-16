19fd97c91ccd7dfb3b1670bf614ad33b27dbc8ca
Dawn Monroe <deetriplett@gmail.com>
2017-02-15 20:51:47 -0500

    Add files via upload

--- Game.cs
+++ Game.cs
@@ -22,6 +22,29 @@ namespace TreehouseDefense
                         new MapLocation(7, 2, map)
                     }
                 );
+                
+                Invader[] invaders = 
+                {
+                    new Invader(path),
+                    new Invader(path),
+                    new Invader(path),
+                    new Invader(path)
+                };
+                
+                Tower[] towers = {
+                    new Tower(new MapLocation(1, 3, map)),
+                    new Tower(new MapLocation(3, 3, map)),
+                    new Tower(new MapLocation(5, 3, map))
+                };
+                
+                Level leve1 = new Level(invaders)
+                {
+                    Towers = towers
+                };
+                
+                bool playerWon = leve1.Play();
+                
+                Console.WriteLine("Player " + (playerWon? "won" : "lost"));
             }
             catch(OutOfBoundsException ex)
             {
