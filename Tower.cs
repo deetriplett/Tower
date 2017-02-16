538fd5c5968f3ccc621c8772d7064ebe2a0899fa
Dawn Monroe <deetriplett@gmail.com>
2017-02-15 20:53:22 -0500

    Add files via upload
    need to update to castle and tower

--- Tower.cs
+++ Tower.cs
@@ -1,11 +1,21 @@
+using System;
+
 namespace TreehouseDefense
 {
     class Tower
     {
         private const int _range = 1;
         private const int _power = 1;
+        private const double _accuracy = .75;
+        
+        private static readonly Random _random = new Random();
         
         private readonly MapLocation _location;
+        
+        public bool IsSuccessfulShot()
+        {
+            return _random.NextDouble() < _accuracy;
+        }
         
         public Tower(MapLocation location)
         {
@@ -18,7 +28,19 @@ namespace TreehouseDefense
             {
                 if(invader.IsActive && _location.InRangeOf(invader.Location, _range))
                 {
-                    invader.DecreaseHealth(_power);
+                    if(IsSuccessfulShot())
+                    {
+                        invader.DecreaseHealth(_power);
+                        Console.WriteLine("Shot at and hit an invader!");
+                        if(invader.IsNeutralized)
+                        {
+                            Console.WriteLine("Neutralized an invader!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Shot at and missed an invader");
+                    }
                     break;
                 }
             }
