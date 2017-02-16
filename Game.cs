61113c29de28d208c66a84657c3c2f057c323e9f
Dawn Monroe <deetriplett@gmail.com>
2017-02-15 19:02:56 -0500

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace TreehouseDefense
+namespace CastleDefense
 {
     class Game
     {
@@ -27,9 +27,9 @@ namespace TreehouseDefense
             {
                 Console.WriteLine(ex.Message);
             }
-            catch(TreehouseDefenseException)
+            catch(CastleDefenseException)
             {
-                Console.WriteLine("Unhandled TreehouseDefenseException");
+                Console.WriteLine("Unhandled CastleDefenseException");
             }
             catch(Exception ex)
             {
